4a6b669b46c19e5187709235e59a9a378cd5ab66
glidergeek <GliderGeek@users.noreply.github.com>
2023-02-17 13:29:50 +0100

    README-pypi gave issues, try without it

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ about = {}
 with open(Path(__file__).parent / 'bro' / '__version__.py', 'r') as f:
     exec(f.read(), about)
 
-long_description = (Path(__file__).parent / 'README-PYPI.md').read_text()
-
 setup(
     name='bro-api',
     version=about['__version__'],
     description='Open source python library for accessing BRO API',
-    long_description=long_description,
+    long_description='this is a long description',
     long_description_content_type="text/markdown",
     url='https://github.com/viktor-platform/bro-api',
     author='VIKTOR',
