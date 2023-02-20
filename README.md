0bf72e4646dd5c662fb4b2fad656a2f70bc762eb
Matthijs <GliderGeek@users.noreply.github.com>
2023-02-20 15:26:18 +0100

    Change package (#3)

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# Publish
+# bro
+put description here
+
+## Installation
+
+```python
+pip install bro
+```
+
+## Use
+
+```python
+
+from bro.temp import temp_func
+
+temp_func()
+```
+
+
+## Publish
 - update changelog with version number
 - update bro/__version__.py
 - tag with version number
--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open(Path(__file__).parent / 'bro' / '__version__.py', 'r') as f:
     exec(f.read(), about)
 
 setup(
-    name='bro-api',
+    name='bro',
     version=about['__version__'],
     description='Open source python library for accessing BRO API',
     long_description='this is a long description',
     long_description_content_type="text/markdown",
-    url='https://github.com/viktor-platform/bro-api',
+    url='https://github.com/viktor-platform/bro',
     author='VIKTOR',
     author_email='support@viktor.ai',
     license='MIT',
