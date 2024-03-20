9aba34850d84dac7b47fedd7ce84cc9105f471b5
Wichard Bron <wichard.bron@wsp.com>
2024-03-20 12:50:23 +0100

    add __version__ to bro.__init__ to make it readable for pyproject.toml

--- bro/__init__.py
+++ bro/__init__.py
@@ -1,4 +1,6 @@
-from bro.__version__ import __version__
+import bro.__version__ as _version
 from bro.api import *
 from bro.helper_functions import *
 from bro.objects import *
+
+__version__ = _version.__version__
