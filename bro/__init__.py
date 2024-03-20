a11d56816c7c831a6d2601790a94f3391a0e6306
Wichard Bron <wichard.bron@wsp.com>
2024-03-20 13:11:02 +0100

    add comment

--- bro/__init__.py
+++ bro/__init__.py
@@ -3,4 +3,5 @@ from bro.api import *
 from bro.helper_functions import *
 from bro.objects import *
 
+# has to be set explicitly as attribute to be accessible from pyproject.toml # TODO: pyproject.tom can also get version directly from git tag, sounds like a better idea
 __version__ = _version.__version__
