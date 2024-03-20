4c701cdbd38a6d3f8f479067a434f6160896f89c
Wichard Bron <wichard.bron@wsp.com>
2024-03-20 14:06:11 +0100

    fix version readout for build

--- bro/__init__.py
+++ bro/__init__.py
@@ -1,7 +1,3 @@
-import bro.__version__ as _version
 from bro.api import *
 from bro.helper_functions import *
 from bro.objects import *
-
-# has to be set explicitly as attribute to be accessible from pyproject.toml # TODO: pyproject.tom can also get version directly from git tag, sounds like a better idea
-__version__ = _version.__version__
